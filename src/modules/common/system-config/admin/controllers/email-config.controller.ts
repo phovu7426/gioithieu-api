2cf2d61ee0adc847b68d0266ce4242473b12e074
hoangvm-vhv <hoangvm@vhv.vn>
2026-01-17 17:30:08 +0700

    sửa đường dẫn system-configs admin

--- src/modules/common/system-config/admin/controllers/email-config.controller.ts
+++ src/modules/common/system-config/admin/controllers/email-config.controller.ts
@@ -11,12 +11,12 @@ import { LogRequest } from '@/common/decorators/log-request.decorator';
 import { Permission } from '@/common/decorators/rbac.decorators';
 import { AuthService } from '@/common/services/auth.service';
 
-@Controller('admin/system-config/email')
+@Controller('admin/system-configs/email')
 export class EmailConfigController {
   constructor(
     private readonly emailConfigService: EmailConfigService,
     private readonly auth: AuthService,
-  ) {}
+  ) { }
 
   /**
    * Lấy cấu hình email
--- src/modules/common/system-config/admin/controllers/general-config.controller.ts
+++ src/modules/common/system-config/admin/controllers/general-config.controller.ts
@@ -11,12 +11,12 @@ import { LogRequest } from '@/common/decorators/log-request.decorator';
 import { Permission } from '@/common/decorators/rbac.decorators';
 import { AuthService } from '@/common/services/auth.service';
 
-@Controller('admin/system-config/general')
+@Controller('admin/system-configs/general')
 export class GeneralConfigController {
   constructor(
     private readonly generalConfigService: GeneralConfigService,
     private readonly auth: AuthService,
-  ) {}
+  ) { }
 
   /**
    * Lấy cấu hình chung
